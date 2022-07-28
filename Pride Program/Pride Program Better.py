2e9ba1fe549e0c6e973f8949dfed9e419ddd6d74
Lily <lilillersanne@gmail.com>
2022-07-28 16:56:09 -0700

    Reads the file now

--- Pride Program/Pride Program Better.py
+++ Pride Program/Pride Program Better.py
@@ -2,19 +2,19 @@ from tkinter import *
 import tkinter as tk
 import tkinter.simpledialog
 def read_from_file():
-    with open('Pride Program.txt', 'r')as file:
+    with open('C:\\Users\lilil\Documents\Python Programs\Pride Program\Pride Program.txt') as file:
         for line in file:
             line = line.rstrip('/n');
             name,definition = line.split('/');
             dictionary[name] = definition;
 def write_to_file(name_name, definition_name):
-    with open('Pride Program.txt', 'a') as file:
+    with open('C:\\Users\lilil\Documents\Python Programs\Pride Program\Pride Program.txt', 'a') as file:
         file.write('/n'+ name_name + '/' + definition_name);
 root = tk.Tk();
 root.withdraw();
 dictionary = {};
 read_from_file();
-pride_query = simpledialog.askstring('Pride', 'Name an LGBTQ+ term!');
+pride_query = tkinter.simpledialog.askstring('Pride', 'Name an LGBTQ+ term!');
 if pride_query in dictionary:
     result = dictionary[pride_query];
-    messagebox.showinfo('Definition', result);
+    tkinter.messagebox.showinfo('Definition', result);
