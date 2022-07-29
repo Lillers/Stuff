702e2771412a1a0a998a10d5bd39bb55c038c87d
Lily <lilillersanne@gmail.com>
2022-07-29 13:07:05 -0700

    Write to mod list

--- Pride Program/Pride Program Better.py
+++ Pride Program/Pride Program Better.py
@@ -8,9 +8,9 @@ def read_from_file():
             name,definition = line.split('/');
             dictionary[name] = definition;
 def write_to_file(name_name, definition_name):
-    with open('C:\\Users\lilil\Documents\Python Programs\Pride Program\Pride Program.txt', 'a') as file:
-        file.write('/n'+ name_name + '/' + definition_name);
-root = tk.Tk();
+    with open('C:\\Users\lilil\Documents\Python Programs\Pride Program\Pride Mod List.txt', 'a') as file:
+        file.write(' ' + name_name + '/' + definition_name + ' ')
+root = tk.Tk()
 root.withdraw();
 dictionary = {};
 read_from_file();
@@ -19,3 +19,7 @@ pride_query = pride_query.upper()
 if pride_query in dictionary:
     result = dictionary[pride_query];
     tkinter.messagebox.showinfo('Definition', pride_query + ' is ' + result);
+else:
+    new_definition = tkinter.simpledialog.askstring ('Teach me', "I don't know that one. Teach me the definition of " + pride_query + '!')
+    dictionary[pride_query] = new_definition
+    write_to_file(pride_query, new_definition)
